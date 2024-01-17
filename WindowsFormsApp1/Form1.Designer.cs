37047adb23c5a016149e3d3138ce7e5623a8e7f8
Валерий Астроух <valera.astroukh@mail.ru>
2024-01-17 17:25:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "...";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ФИО получателя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Название компании";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Дата повышения цены";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Наименование услуги";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(173, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "...";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(173, 166);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(144, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "...";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(408, 40);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(366, 272);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(173, 124);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(145, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(173, 208);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "Астроух В.Д.";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ФИО отправителя";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Fill()
+        {
+            string document_text = String.Format(
+                "Уважаемый {0}, \r\n" +
+                "Компания \"{1}\" благодарит Вас за сотрудничество, выражает глубокую признательность за верность и доверие, которое Вы оказываете нам, пользуясь нашими продуктами и методами. " +
+                "Для поддерживания высокого качества услуг мы постоянно инвестируем средства на усовершенствование методоа и разработку новых. \r\n" +
+                "Наряду с этим, из-за повышения количества желаюхих обучиться и расширения обучающей базы, мы вынужденны повысить цены на обучение и некоторые продукты. \r\n" +
+                "Мы хотим подчеркнуть, что данный шаг необходим, прежде всего, для обеспечения стабильности, качества услуг и продуктов. \r\n\r\n" +
+                "С {2} повысилась цена на услугу \"{3}\". \r\n\r\n" +
+                "C уважением,\r\n " +
+                "{4}.",
+                textBox1.Text,
+                textBox2.Text,
+                dateTimePicker1.Text,
+                textBox4.Text,
+                textBox3.Text
+                );
+            return document_text;
+        }
+
+        public bool Check()
+        {
+            bool flag = true;
+            foreach (var tb in this.Controls.OfType<TextBox>())
+            {
+                if (tb.Text == "" || tb.Text == "...")
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox5.Text = Fill();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.Text = Fill();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.Text = Fill();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.Text = Fill();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.Text = Fill();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Check())
+            {
+                Object Template = Type.Missing;
+                Object NewTemplate = Type.Missing;
+                Object Typpe = Type.Missing;
+                Object Visible = Type.Missing;
+                var Word1 = new Microsoft.Office.Interop.Word.Application();
+                Word1.Visible = true;
+                var newDocument = Word1.Documents.Add(ref Template, ref NewTemplate, ref Typpe, ref Visible);
+
+                newDocument.Words.First.InsertBefore(Fill());
+
+                foreach (Paragraph paragrph in newDocument.Paragraphs)
+                {
+                    paragrph.Range.Font.Color = WdColor.wdColorBlack;
+                    paragrph.Range.Font.Size = 16;
+                    paragrph.Range.Font.Name = "Times New Roman";
+                    paragrph.Range.Font.Italic = 0;
+                    paragrph.Range.Font.Bold = 0;
+                }
+
+                object begin = 0;
+                object end = 0;
+                Range wordrange = null;
+
+                begin = Fill().IndexOf(textBox1.Text);
+                end = (int)begin + textBox1.Text.Length;
+                wordrange = newDocument.Range(ref begin, ref end);
+                wordrange.Select();
+                wordrange.Font.Italic = 1;
+
+                begin = Fill().IndexOf(textBox2.Text);
+                end = (int)begin + textBox2.Text.Length;
+                wordrange = newDocument.Range(ref begin, ref end);
+                wordrange.Select();
+                wordrange.Font.Italic = 1;
+
+                begin = Fill().IndexOf(dateTimePicker1.Text);
+                end = (int)begin + dateTimePicker1.Text.Length;
+                wordrange = newDocument.Range(ref begin, ref end);
+                wordrange.Select();
+                wordrange.Font.Italic = 1;
+
+                begin = Fill().IndexOf(textBox4.Text);
+                end = (int)begin + textBox4.Text.Length;
+                wordrange = newDocument.Range(ref begin, ref end);
+                wordrange.Select();
+                wordrange.Font.Italic = 1;
+
+                Object FileName = @"D:\" + textBox1.Text + ".doc";
+                Object FileFormat = Type.Missing;
+                Object LockComment = Type.Missing;
+                Object Password = Type.Missing;
+                Object AddToResentFile = Type.Missing;
+                Object WritePass = Type.Missing;
+                Object ReadOnlyRecommended = Type.Missing;
+                Object EmbedTrue = Type.Missing;
+                Object SaveNative = Type.Missing;
+                Object SaveFormsData = Type.Missing;
+                Object SaveAs = Type.Missing;
+                Object Encoding = Type.Missing;
+                Object InsertLine = Type.Missing;
+                Object AllowSub = Type.Missing;
+                Object LineEnd = Type.Missing;
+                Object AddBi = Type.Missing;
+                Object SaveChang = Type.Missing;
+                Object OriginalFormat = Type.Missing;
+                Object RouteDoc = Type.Missing;
+                Word1.ActiveDocument.SaveAs(ref FileName,
+                    ref FileFormat,
+                    ref LockComment,
+                    ref Password,
+                    ref AddToResentFile,
+                    ref WritePass,
+                    ref ReadOnlyRecommended,
+                    ref EmbedTrue,
+                    ref SaveNative,
+                    ref SaveFormsData,
+                    ref SaveAs,
+                    ref Encoding,
+                    ref InsertLine,
+                    ref AllowSub,
+                    ref LineEnd,
+                    ref AddBi);
+                Word1.Application.Quit(ref SaveChang, ref OriginalFormat, ref RouteDoc);
+            }
+            else
+            {
+                MessageBox.Show("Заполните все необходимые поля");
+            }
+        }
+    }
+}
